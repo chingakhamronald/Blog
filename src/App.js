9a43fb6adb883c72cd3cb7d8279493f5980f070a
chingakhamronald <ronaldoch.lamzing@gmail.com>
2023-01-19 00:03:33 +0530

    state changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 
+const data = ["Eat", "Sleep", "Code"];
+
 const App = () => {
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      const idx = Math.floor(Math.random() * data.length);
+      setTitle(data[idx]);
+    }, 1000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [title]);
+
   return (
     <Box
       display={"flex"}
@@ -11,7 +26,7 @@ const App = () => {
       height={"100%"}
     >
       <Typography variant="h3" color={"InfoText"}>
-        Come
+        I Like to {title}!
       </Typography>
     </Box>
   );
